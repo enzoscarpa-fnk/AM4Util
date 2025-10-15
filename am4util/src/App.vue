b7afa9516a4880d804d9d014511a43ddf70ac6f7
enzoscarpa-fnk <159703846+enzoscarpa-fnk@users.noreply.github.com>
2025-10-16 01:35:09 +0200

    Added "report bug/enhance" link

--- am4util/src/App.vue
+++ am4util/src/App.vue
@@ -9,6 +9,9 @@ import SeatOptimizer from "@/components/SeatOptimizer.vue";
       <div class="grid md:grid-cols-2 gap-3">
         <TicketCalculator />
         <SeatOptimizer />
+        <div class="text-sm text-rose-400 underline text-center">
+          <a href="https://github.com/enzoscarpa-fnk/AM4Util/issues" target="_blank" rel="noopener noreferrer">Report a bug or help us enhance this tool</a>
+        </div>
       </div>
     </div>
   </div>
